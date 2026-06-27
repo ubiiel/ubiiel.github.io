e9ae568cbfe54671956ec687dd1d07d75d93cf96
Gabriel Matos <81238494+ubiiel@users.noreply.github.com>
2026-06-27 18:56:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,35 @@
-//ANIMACAO AO CLICAR NO CHECK
-//SALVA O VALOR
-//RESETA O TEMPO DE SELECAO
-//CONSOLE MOSTRA DADOS SALVOS
-//LIMPA A CAIXA DE INPUT
-//CRIA A DIV 
-//INSERE A DIV NA LISTA
-
 
 const check = document.querySelector('.fa-check'); //CONSTANTE CHECK
 const txtInput = document.querySelector('#caixa'); //CONSTANTE PARA REFERIR AO TEXTO DO INPUT
 const listBox = document.querySelector('.box');  //CONSTANTE PARA ADICIONAR O TEXTO NA BOX
 const listaVazia = document.querySelector('#nulltxt') //CONSTANTE PARA REMOVER O NULL QUANDO VAZIA
 
-
-txtInput.addEventListener('keydown', function (evento){
-    if (evento.key === "Enter") {
-        check.click()
-    }
-})
-check.addEventListener('click', function () {
-    check.classList.add('clicado'); //MUDANCA DE COR DO CHECK
-    let textosalvo = txtInput.value.trim(); //SALVA O TEXTO DIGITADO NA VARIAVEL LET 'TEXTOSALVO' //TRIM DESCONSIDERA ESPAÇOS NO COMEÇO E NO FIM
+function adicionarLista() {
+     let textosalvo = txtInput.value.trim(); //SALVA O TEXTO DIGITADO NA VARIAVEL LET 'TEXTOSALVO' //TRIM DESCONSIDERA ESPAÇOS NO COMEÇO E NO FIM
     if (textosalvo === "") {
         fundo.classList.remove('fechado')
-        txtInput.clear()
+        txtInput.blur() //tira o foco do input com o blur; desseleciona o input;
         return;
-    }
-    listaVazia.classList.add('sumido') //APAGA O LISTA VAZIA QUANDO ADICIONADO UM ITEM NA LISTA
-    setTimeout(() => {
-        check.classList.remove('clicado')
-    }, 1100); //TEMPO DE RESET DE ELEMENTO PARA PODER FAZER SEMPRE A ANIMAÇAO
-    caixa.value = ""; //LIMPA A CAIXA DEPOIS DE ADICIONAR NA LISTA
+    } else {
 
-    const novoItem = document.createElement('div'); //CONSTANTE PARA CRIAR UM NOVO ITEM QUANDO DIGITADO
-    novoItem.classList.add('item-lista'); //MANIPULA O novoItem TRANSFORMANDO ELE NA CLASSE '.item-lista'
-    listBox.prepend(novoItem); //INSERE A DIV NA PAGINA
-    novoItem.innerHTML = `
-    <input type="checkbox" id="checkbox">
-    <span>${textosalvo}</span>`; //ADICIONA A CHECKBOX E O ITEM ESCRITO NA LISTA
-
-    const checkbox = novoItem.querySelector('input')
-    
-    checkbox.addEventListener('change', function () {
-        if (checkbox.checked) {
-            listBox.appendChild(novoItem)
-        }
-        else {
-            const itemMarcado = listBox.querySelector('.item-lista input:checked')
+        //funcionalidade completa de adicionar item e o posicionamento das marcadas e desmarcadas
+        const novoItem = document.createElement('div'); //CONSTANTE PARA CRIAR UM NOVO ITEM QUANDO DIGITADO
+        listaVazia.classList.add('sumido') //APAGA O LISTA VAZIA QUANDO ADICIONADO UM ITEM NA LISTA
+        novoItem.classList.add('item-lista'); //MANIPULA O novoItem TRANSFORMANDO ELE NA CLASSE '.item-lista'
+        listBox.prepend(novoItem); //INSERE A DIV NA PAGINA
+        novoItem.innerHTML = `
+        <input type="checkbox" id="checkbox">
+        <span>${textosalvo}</span>`; //ADICIONA A CHECKBOX E O ITEM ESCRITO NA LISTA
+        caixa.value = ""; //LIMPA A CAIXA DEPOIS DE ADICIONAR NA LISTA
+        //POSICONAMENTO DAS MARCADAS E DESMARCADAS
+        const checkbox = novoItem.querySelector('input')
+        
+        checkbox.addEventListener('change', function () {
+            if (checkbox.checked) {
+                listBox.appendChild(novoItem)
+            }
+            else {
+                const itemMarcado = listBox.querySelector('.item-lista input:checked')
             if (itemMarcado) {
                 const divItemMarcado = itemMarcado.closest('.item-lista')
                 listBox.insertBefore(novoItem, divItemMarcado)
@@ -54,15 +37,39 @@ check.addEventListener('click', function () {
             else {
                 listBox.append(novoItem)
             }
-    
+
         }
     })
+}
+};
+
+
+
+
+//funcionamento de adiconar com tecla a "Enter"
+txtInput.addEventListener('keydown', function (evento) {
+    if (evento.key === "Enter") {
+        adicionarLista();
+
+        //animacao da check - copiado
+        check.classList.add('clicado'); //MUDANCA DE COR DO CHECK
+    setTimeout(() => {
+        check.classList.remove('clicado')
+    }, 1100); //TEMPO DE RESET DE ELEMENTO PARA PODER FAZER SEMPRE A ANIMAÇAO
+    }
+})
+
+
+//funcionamento para adicionar via clique na "check"
+check.addEventListener('click', function () {
+    adicionarLista(); //chamar funcao para adicionar na lista
+
+    //animacao da check
+    check.classList.add('clicado'); //MUDANCA DE COR DO CHECK
+    setTimeout(() => {
+        check.classList.remove('clicado')
+    }, 1100); //TEMPO DE RESET DE ELEMENTO PARA PODER FAZER SEMPRE A ANIMAÇAO
 });
-
-
-
-
-
 
 
 
